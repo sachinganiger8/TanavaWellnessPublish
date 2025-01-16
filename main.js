bc2802f95b12fb80b5450925f44d6728702c1d53
sachinganiger8 <sachinganiger8@gmail.com>
2025-01-17 00:56:29 +0530

    added first product page

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
+    debugger;
+    let navElmnts = ["#body", "#products", "#about-us", "#contact-us"];
+    let hasNavEl = false;
+    for (let index = 0; index < navElmnts.length; index++) {
+        const navEl = navElmnts[index];
+        if (window.location.href.includes(navEl)) {
+            hasNavEl = true;
+            $(".navbar-nav .nav-link").removeClass("active");
+            $(".nav-link[href='" + navEl + "']").addClass("active");
+            break;
+        }
+    }
     $(".navbar-nav .nav-link").click(function (e) {
-        e.preventDefault();
-        $(".navbar-nav .nav-link").removeClass("active");
-        $(this).addClass("active");
         let scrollToElId = $(this).attr("href");
-        $('html, body').animate({
-            scrollTop: $(scrollToElId).offset().top
-        }, 500);
+
+        if ($(scrollToElId).length > 0) {
+            e.preventDefault();
+            $(".navbar-nav .nav-link").removeClass("active");
+            $(this).addClass("active");
+            $('html, body').animate({
+                scrollTop: $(scrollToElId).offset().top
+            }, 500);
+        }
     });
 });
